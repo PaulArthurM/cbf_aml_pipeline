ff572ef45d6f7c1b67cd0efabac470619417051b
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-01 12:34:47 +0200

    Test: remove conda activate

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -19,8 +19,8 @@ echo "NSLOTS: $NSLOTS"
 
 echo Start at:`date`
 
-source activate smk_env
+#source activate smk_env
 snakemake
-source deactivate smk_env
+#source deactivate smk_env
 
 echo End at:`date`
