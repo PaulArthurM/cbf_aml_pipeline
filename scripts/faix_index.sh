b233c324f9e9464c190f30bfeb57cb6238f1157f
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-10 16:27:17 +0200

    Add script for index fasta file with samtools faidx

---
+++ scripts/faix_index.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N samtools_faidx
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=01:00:00
+#$ -pe thread 4
+#$ -l h_vmem=11G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+conda activate samtools_env
+samtools faidx ${1}
+conda deactivate
+
+echo End at:`date`
