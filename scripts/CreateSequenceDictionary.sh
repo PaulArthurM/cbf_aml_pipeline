72280ec409de51598376335b8e5112422a0ebb4f
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-10 16:13:55 +0200

    Add bash script for CreateSequenceDictionary

---
+++ scripts/CreateSequenceDictionary.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N createSequenceDictionary
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=01:00:00
+#$ -pe thread 1
+#$ -l h_vmem=2.75G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+conda activate gatk4_4.1.2.0_env
+gatk CreateSequenceDictionary -R ${1} -O ${2}.dict
+conda deactivate
+
+echo End at:`date`
