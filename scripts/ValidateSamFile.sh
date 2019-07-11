81f685d44c2fc267dfa4261ffed07a9c12fb8138
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-11 15:56:39 +0200

    Add script for Validate SAm files

---
+++ scripts/ValidateSamFile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N ValidateSamFile
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=24:00:00
+#$ -pe thread 6
+#$ -l h_vmem=16.5G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+conda activate gatk4_4.1.2.0_env
+gatk ValidateSamFile \
+  I=${1} \
+  MODE=SUMMARY
+conda deactivate
+
+echo End at:`date`
