6559743bbab30cdd342739b4fa5c3b9da84df785
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-11 11:18:17 +0200

    Add bash script for IndexFeaturesFile

---
+++ scripts/IndexFeatures.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N IndexFeatures
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=01:00:00
+#$ -pe thread 4
+#$ -l h_vmem=11G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+conda activate gatk4_4.1.2.0_env
+gatk IndexFeatureFile \
+     -F ${1}
+conda deactivate
+
+echo End at:`date`
