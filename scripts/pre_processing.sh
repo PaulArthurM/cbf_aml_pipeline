bedf58f3b4e647390f31b931e4287f91130a9243
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-11 11:13:32 +0200

    Remove --unlock

--- scripts/pre_processing.sh
+++ scripts/pre_processing.sh
@@ -20,7 +20,7 @@ echo "NSLOTS: $NSLOTS"
 echo Start at:`date`
 
 conda activate smk_env
-snakemake -s scripts/pre_processing.smk --unlock
+snakemake -s scripts/pre_processing.smk
 conda deactivate
 
 echo End at:`date`
