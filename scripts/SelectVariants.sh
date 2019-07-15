d76bc1d5d2e4af1a780680bf8384cb9ca3f6be89
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-15 16:21:23 +0200

    Change command

--- scripts/SelectVariants.sh
+++ scripts/SelectVariants.sh
@@ -23,7 +23,7 @@ echo Variant file: ${1}
 echo Output VCF: ${2}
 
 conda activate gatk4_4.1.2.0_env
-gatk --java-options -Xmx4G SelectVariants -V ${1} -select-type SNP -restrict-alleles-to BIALLELIC -O ${2}.vcf.gz
+gatk SelectVariants -V ${1} -select-type SNP -restrict-alleles-to BIALLELIC -O ${2}.vcf.gz
 conda deactivate
 
 echo End at:`date`
