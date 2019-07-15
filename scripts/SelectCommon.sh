d283e6852f71d6b06af14d47df15751417dba491
paulArthurMsl <paularth.meslin@gmail.com>
2019-07-15 14:46:38 +0200

    Add bash script for selection of common variants

---
+++ scripts/SelectCommon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N unzip
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=01:00:00
+#$ -pe thread 4
+#$ -l h_vmem=11G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+gatk SelectVariants -V ${1} -L 1 --select "AF > 0.05" -O ${2}
+
+echo End at:`date`
