a49bec3098c79248daf2482171679f68c058c048
paulArthurMsl <paularth.meslin@gmail.com>
2019-10-28 10:00:06 +0100

    Add script for fastqc test

---
+++ scripts/fastqc_qsub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N fastqc_test
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=01:00:00
+#$ -pe thread 1
+#$ -l h_vmem=2.75G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+fastqc data/bam/SJCBF016_G.5.6_marked_duplicates_BQSR_merge.bam -o analysis/qualities/
+
+echo End at:`date`
