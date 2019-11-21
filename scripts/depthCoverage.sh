ef19a460dc62823e791194d33a9082d16768cfdd
paulArthurMsl <paularth.meslin@gmail.com>
2019-11-21 15:00:35 +0100

    Add script for depth of coverage

---
+++ scripts/depthCoverage.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N DepthOfCoverage_SJCBF040
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=24:00:00
+#$ -pe thread 5
+#$ -l h_vmem=2.75G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+conda activate gatk_env
+
+gatk3 -T DepthOfCoverage \
+  -omitBaseOutput \
+  -omitLocusTable\
+  -R /data1/scratch/pamesl/projet_cbf/data/b37_data/GRCh37-lite.fa \
+  -I /data/bam/SJCBF040_D.6.7_marked_duplicates_BQSR_merge.bam \
+  -L /data1/scratch/pamesl/projet_cbf/data/b37_data/Broad.human.exome.b37.interval_list \
+  -o SJCBF040_D.coverage \
+  -ct 5 -ct 10 -ct 15 -ct 20 -ct 30 -ct 50
+
+conda deactivate
+
+echo End at:`date`
