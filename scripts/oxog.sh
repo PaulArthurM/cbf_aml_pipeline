42ff8941d0e7f4de1fbef67dc0533c224f4f4248
paulArthurMsl <paularth.meslin@gmail.com>
2019-11-22 15:28:35 +0100

    Add oxog script

---
+++ scripts/oxog.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N samtools_index
+#$ -cwd
+#$ -o /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -e /data1/scratch/pamesl/projet_cbf/stdoe_sge
+#$ -q short.q
+#$ -l h_rt=01:00:00
+#$ -pe thread 1
+#$ -l h_vmem=2.75G
+
+echo "JOB NAME: $JOB_NAME"
+echo "JOB ID: $JOB_ID"
+echo "QUEUE: $QUEUE"
+echo "HOSTNAME: $HOSTNAME"
+echo "SGE O WORKDIR: $SGE_O_WORKDIR"
+echo "SGE TASK ID: $SGE_TASK_ID"
+echo "NSLOTS: $NSLOTS"
+
+echo Start at:`date`
+
+conda activate RNAseq_pipeline_Gaetano
+
+picard CollectOxoGMetrics \
+      I=/data1/scratch/pamesl/projet_cbf/data/bam/SJCBF040_D.6.7_marked_duplicates_BQSR_merge.bam \
+      O=/data1/scratch/pamesl/projet_cbf/data/oxog/oxoG_SJCBF040_metrics.txt \
+      R=/data1/scratch/pamesl/projet_cbf/data/b37_data/GRCh37-lite.fa
+
+conda deactivate
+
+echo End at:`date`
