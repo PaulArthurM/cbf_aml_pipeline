6d58c20d57ba2f78e813e977062b49e364b057b2
paulArthurMsl <paularth.meslin@gmail.com>
2020-02-03 15:45:53 +0100

    Add comptage scripts

---
+++ workflow/scripts/compteGroupes.py
@@ -0,0 +1,24 @@
+
+
+
+file_sample = "/data1/scratch/pamesl/cbf_aml_pipeline/Sample_File_SJCBF.map"
+fo_sample = open(file)
+samples = fo.readlines()
+
+file = "/data1/scratch/pamesl/cbf_aml_pipeline/validated_SJCBF.tsv"
+f_sample = open(file)
+variants = f_sample.readlines()
+
+
+dico = {}
+
+for variant in variants:
+    sample = variant.split("\t")[2]
+    if sample in samples:
+        if sample in dico:
+            dico[sample] += 1
+        elif not sample in dico:
+            dico[sample] = 1
+
+
+print(dico)
