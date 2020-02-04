d2a5066daecc9ea119452f93314be9a18c1a76f9
paulArthurMsl <paularth.meslin@gmail.com>
2020-02-04 11:01:09 +0100

    Add Somatic Sniper

---
+++ workflow/rules/somaticSniper.smk
@@ -0,0 +1,21 @@
+
+rule somatic-sniper:
+    input:
+        normal = "results/preprocessing/{sample}_G.bam",
+        tumor = "results/preprocessing/{sample}_D.bam"
+    output:
+        "results/variantCalling/Somatic-sniper/{sample}_somatic-sniper.snv"
+    params:
+        name="somatic-sniper_{sample}",
+        nthread=5,
+        ref = config["reference_GRCh37-lite"]
+    conda:
+        "../envs/somaticsniper.yaml"
+    shell:
+        "bam-somaticsniper \
+            -q 40 \
+            -Q 40 \
+            -f {params.ref} \
+            {input.tumor} \
+            {input.normal} \
+            {output}"
--- workflow/scripts/compteGroupes.py
+++ workflow/scripts/compteGroupes.py
@@ -1,25 +1,60 @@
 
 
 
-file_sample = "/data1/scratch/pamesl/cbf_aml_pipeline/Sample_File_SJCBF.map"
+file_sample = "/home/puissant/Sample_list.txt"
 fo_sample = open(file_sample)
-samples = fo_sample.readlines()
+l = fo_sample.readlines()
+samples = []
+for ll in l:
+    sample = ll[0:-1]
+    samples.append(sample)
 
-file = "/data1/scratch/pamesl/cbf_aml_pipeline/validated_SJCBF.tsv"
+
+
+file = "/home/puissant/validated_SJCBF.tsv"
 f_sample = open(file)
-variants = f_sample.readlines()
+l = f_sample.readlines()
+variants = []
+for ll in l:
+    s = ll.split("\t")
+    if len(s) > 2:
+        s[2] = s[2][0:-1]
+        variants.append(s)
 
 
 dico = {}
+sign = ["KIT", "FLT3", "NRAS", "KRAS", "CBL", "JAK2"]
+
+for sample in samples:
+    dico[sample] = 0
 
 for variant in variants:
-    split = variant.split("\t")
-    if len(split) > 2:
-        if sample in samples:
-            if sample in dico:
-                dico[sample] += 1
-            elif not sample in dico:
-                dico[sample] = 1
-
+    if variant[2] in samples:
+        if variant[0] in sign:
+            dico[variant[2]] += 1
 
 print(dico)
+
+no = 0
+single = 0
+int_clo = 0
+for sample in dico:
+    if dico[sample] == 0:
+        no += 1
+    if dico[sample] == 1:
+        single += 1
+    if dico[sample] > 1:
+        int_clo += 1
+
+for sample in dico:
+    #if dico[sample] == 0:
+        #print(sample)
+    #if dico[sample] == 1:
+        #print(sample)
+    if dico[sample] > 1:
+        print(sample)
+
+if 0:
+    print(no)
+    print(single)
+    print(int_clo)
